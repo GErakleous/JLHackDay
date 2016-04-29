663ae6a6da1c06d4e4bdbe1ba22457ca3375fef4
Emma Lee-Choon <eleechoon@gmail.com>
2016-04-29 16:15:27 +0100

    Add view controllers for photos and lamps

--- JohnLewis/JohnLewis/Classes/Scenes/Result/JLResultSceneViewController.m
+++ JohnLewis/JohnLewis/Classes/Scenes/Result/JLResultSceneViewController.m
@@ -12,12 +12,20 @@
 #import "JLSwipeCollectionViewCell.h"
 
 static NSString * const kJLCellIdentifier =	@"JLCollectionViewCell";
+static NSString * const kJLPrintCellIdentifier =	@"JLPrintCollectionViewCell";
+static NSString * const kJLLampCellIdentifier =	@"JLLampCollectionViewCell";
 
 @interface JLResultSceneViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 
 @property (nonatomic, strong) UICollectionView *collectionViewTutorial;
+@property (nonatomic, strong) UICollectionView *printCollectionView;
+@property (nonatomic, strong) UICollectionView *lampCollectionView;
 @property (nonatomic, strong) NSArray *sofaImages;
 @property (nonatomic, strong) NSArray *sofaDetails;
+@property (nonatomic, strong) NSArray *printImages;
+@property (nonatomic, strong) NSArray *printDetails;
+@property (nonatomic, strong) NSArray *lampImages;
+@property (nonatomic, strong) NSArray *lampDetails;
 @property (nonatomic, strong) UIButton *btnShare;
 
 @end
@@ -64,7 +72,23 @@ static NSString * const kJLCellIdentifier =	@"JLCollectionViewCell";
 #pragma mark - Data Setup
 - (void)setupData {
 	_sofaImages = @[[UIImage imageNamed:@"sofa1.png"], [UIImage imageNamed:@"sofa2.png"], [UIImage imageNamed:@"sofa3.png"], [UIImage imageNamed:@"sofa4.png"], [UIImage imageNamed:@"sofa5.png"], [UIImage imageNamed:@"sofa6.png"]];
-	_sofaDetails = @[@{@"title": @"Title Here", @"Price": @"100.0"}, @{@"title": @"Title Here", @"Price": @"100.0"}, @{@"title": @"Title Here", @"Price": @"100.0"}, @{@"title": @"Title Here", @"Price": @"100.0"}, @{@"title": @"Title Here", @"Price": @"100.0"}, @{@"title": @"Title Here", @"Price": @"100.0"}];
+    _sofaDetails = @[@{@"title": @"John Lewis Cromwell Grand Chesterfield Sofa", @"price":  @"£1599"},
+                     @{@"title": @"John Lewis Inverness Grand Loose Cover Sofa", @"price":  @"£1800"},
+                     @{@"title": @"Duresta Two Seater Sofa", @"price":  @"£499"},
+                     @{@"title": @"John Lewis Harrison Medium Sofa", @"price":  @"£849"},
+                     @{@"title": @"John Lewis Bingham Medium Sofa", @"price":  @"£849"},
+                     @{@"title": @"Collins and Hayes Sofa", @"price":  @"£650"}];
+    
+    _printImages = @[[UIImage imageNamed:@"photo1.png"], [UIImage imageNamed:@"photo2.png"], [UIImage imageNamed:@"photo3.png"], [UIImage imageNamed:@"photo4.png"]];
+    _printDetails = @[@{@"title": @"Art Inspired by Music - Winter, Chocolate Frame", @"price":  @"£65"},
+                      @{@"title": @"Art Inspired by Music - Sunshine, Natural Frame", @"price":  @"£65"},
+                      @{@"title": @"Art Inspired by Music - Danziamo, Chocolate Frame", @"price":  @"£65"},
+                      @{@"title": @"Art Inspired by Music - Paris, Black Frame", @"price":  @"£65"}];
+    
+    _lampImages = @[[UIImage imageNamed:@"lamp1.png"], [UIImage imageNamed:@"lamp2.png"], [UIImage imageNamed:@"lamp3.png"]];
+    _lampDetails = @[@{@"title": @"Bethan Gray for John Lewis Noah Floor Lamp", @"price":  @"£290"},
+                     @{@"title": @"John Lewis Clarence Floor Lamp, Washed Grey", @"price":  @"£340"},
+                     @{@"title": @"John Lewis Aki Sheesham Floor Lamp, Natural", @"price":  @"£310"}];
 }
 
 #pragma mark - UI setup
@@ -95,6 +119,35 @@ static NSString * const kJLCellIdentifier =	@"JLCollectionViewCell";
 	[self.view addSubview:collectionView];
 	_collectionViewTutorial = collectionView;
 	
+    UICollectionViewFlowLayout *printFlowLayout = [[UICollectionViewFlowLayout alloc] init];
+    printFlowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    printFlowLayout.minimumInteritemSpacing = 0;
+    printFlowLayout.minimumLineSpacing = 0;
+    
+    UICollectionView *printCollectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:printFlowLayout];
+    [printCollectionView registerClass:[JLSwipeCollectionViewCell class] forCellWithReuseIdentifier:kJLPrintCellIdentifier];
+    printCollectionView.backgroundColor = [UIColor clearColor];
+    printCollectionView.dataSource = self;
+    printCollectionView.delegate = self;
+    printCollectionView.pagingEnabled = YES;
+    printCollectionView.showsHorizontalScrollIndicator = NO;
+    [self.view addSubview:printCollectionView];
+    _printCollectionView = printCollectionView;
+
+    UICollectionViewFlowLayout *lampFlowLayout = [[UICollectionViewFlowLayout alloc] init];
+    lampFlowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    lampFlowLayout.minimumInteritemSpacing = 0;
+    lampFlowLayout.minimumLineSpacing = 0;
+    
+    UICollectionView *lampCollectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:lampFlowLayout];
+    [lampCollectionView registerClass:[JLSwipeCollectionViewCell class] forCellWithReuseIdentifier:kJLLampCellIdentifier];
+    lampCollectionView.backgroundColor = [UIColor clearColor];
+    lampCollectionView.dataSource = self;
+    lampCollectionView.delegate = self;
+    lampCollectionView.pagingEnabled = YES;
+    lampCollectionView.showsHorizontalScrollIndicator = NO;
+    [self.view addSubview:lampCollectionView];
+    _lampCollectionView = lampCollectionView;
 }
 
 - (void)viewWillLayoutSubviews {
@@ -103,18 +156,44 @@ static NSString * const kJLCellIdentifier =	@"JLCollectionViewCell";
 
 - (void)setupLayout {
 	_collectionViewTutorial.frame = CGRectMake(69.0, CGRectGetMidY(self.view.frame) + 20.0f, self.view.frame.size.width * 0.7f, 120.0f);
+    _printCollectionView.frame = CGRectMake(CGRectGetMidX(self.view.frame)-50, 105, 85, 85);
+    _lampCollectionView.frame = CGRectMake(CGRectGetMaxX(self.view.frame)-160, CGRectGetMidY(self.view.frame) -60.0f, 80, 210.0f);
 	_btnShare.frame = CGRectMake(self.view.frame.size.width - 100.0f, 0.0f, 150, 75);
 }
 
 #pragma mark - Collection View Datasource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-	return _sofaImages.count;
+    if (collectionView == self.collectionViewTutorial) {
+        return _sofaImages.count;
+    }
+    else if (collectionView == self.printCollectionView) {
+        return _printImages.count;
+    }
+    else if (collectionView == self.lampCollectionView) {
+        return _lampImages.count;
+    }
+    return 0;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-	JLSwipeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kJLCellIdentifier forIndexPath:indexPath];
-	[cell updateUIWithImage:_sofaImages[indexPath.row] details:_sofaDetails[indexPath.row] pageIndex:indexPath.row];
-	return cell;
+//	JLSwipeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kJLCellIdentifier forIndexPath:indexPath];
+    if (collectionView == self.collectionViewTutorial) {
+        JLSwipeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kJLCellIdentifier forIndexPath:indexPath];
+        [cell updateUIWithImage:_sofaImages[indexPath.row] details:_sofaDetails[indexPath.row] pageIndex:indexPath.row];
+        return cell;
+    }
+    else if (collectionView == self.printCollectionView) {
+        JLSwipeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kJLPrintCellIdentifier forIndexPath:indexPath];
+        [cell updateUIWithImage:_printImages[indexPath.row] details:_sofaDetails[indexPath.row] pageIndex:indexPath.row];
+        return cell;
+    }
+    else if (collectionView == self.lampCollectionView) {
+        JLSwipeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kJLLampCellIdentifier forIndexPath:indexPath];
+        [cell updateUIWithImage:_lampImages[indexPath.row] details:_sofaDetails[indexPath.row] pageIndex:indexPath.row];
+        return cell;
+    }
+    
+	return nil;
 }
 
 #pragma mark - Collection View Delegate
