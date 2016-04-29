7a4f4c3961aa04d846ef43d3a460aaae1ece02bb
Emma Lee-Choon <eleechoon@gmail.com>
2016-04-29 13:53:47 +0100

    Add scene view for results

---
+++ JohnLewis/JohnLewis/Classes/Scenes/Result/JLResultSceneViewController.h
@@ -0,0 +1,15 @@
+//
+//  JLResultSceneViewController.h
+//  JohnLewis
+//
+//  Created by Emma Lee-Choon on 29/04/2016.
+//  Copyright © 2016 Nimbletank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLResultSceneViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIImageView *resultImageView;
+
+@end
---
+++ JohnLewis/JohnLewis/Classes/Scenes/Result/JLResultSceneViewController.m
@@ -0,0 +1,19 @@
+//
+//  JLResultSceneViewController.m
+//  JohnLewis
+//
+//  Created by Emma Lee-Choon on 29/04/2016.
+//  Copyright © 2016 Nimbletank. All rights reserved.
+//
+
+#import "JLResultSceneViewController.h"
+#import "SDWebImage/UIImageView+WebCache.h"
+
+@interface JLResultSceneViewController()
+
+@end
+
+@implementation JLResultSceneViewController
+
+
+@end
